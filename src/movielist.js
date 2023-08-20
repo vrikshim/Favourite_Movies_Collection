ddfb6f6453b8c1c95b488686dc9a37f4bce344c7
Tanishk Goyal <goyaltanishk11112002@gmail.com>
2023-08-20 09:14:25 +0530

    updates

--- src/movielist.js
+++ src/movielist.js
@@ -3,14 +3,14 @@ import React from 'react'
 const Movielist=(props)=>{
   const Favouritecomponent=props.addfavouriteicon;
     return (
-        <> 
+      <div className='d-flex flex-wrap justify-content-center'>
       
            {props.movies.map((movie,index) => (
                <div key={index} className='image-container  justify-content-start m-3'>
 
                <br></br>
                <img src={movie.Poster} alt='movie' />
-               < div 
+               <div 
                onClick={()=>{
                 props.handlefavouriteclick(movie)
                 console.log('icon was touched')
@@ -24,7 +24,7 @@ const Movielist=(props)=>{
             
            )
            )}
-             </>
+             </div>
     )
 }
 export default Movielist
